c8df0a43cd474ff6353eeabf62f58f3ba461d1b5
IlyaMak <ilya.makarov120@gmail.com>
2024-02-15 09:17:17 +0500

    routes

---
+++ app/navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import { View, Text } from 'react-native'
+
+const Navigation: FC = () => {
+	return (
+		<View>
+			<Text>Navigation</Text>
+		</View>
+	)
+}
+
+export default Navigation
